c7931937c056cf133cafd20483980adcf5c320ca
ULADZIMIR <01myit@gmail.com>
2025-04-09 15:16:24 +0300

    Phases of Event Loop: nextTick, setImmediate, and dns module of Node.js.

--- events/loop.ts
+++ events/loop.ts
@@ -1,11 +1,46 @@
 import fs from 'fs'
+import dns from 'dns'
 const log = console.log
+function timestamp ()
+{
+    return performance.now().toFixed( 2 )
+}
+//! sync
+log( 'START', timestamp () )
 
-log( 'START' )
+//! __________ASYNC
+//! Timers
+setTimeout( () => log( 'Timeout 1', timestamp () ), 0 ) //! in next loop
+setTimeout( () =>
+{
+    process.nextTick( () => log( 'nextTick inner', timestamp() ) )
+    log( 'Timeout 2', timestamp() )
+}, 10 ) //! in next loop
 
-setTimeout( () => log( 'Timeout 1' ), 0 ) //! in next loop
-setTimeout( () => log( 'Timeout 2' ), 10 ) //! in next loop
-fs.writeFile( './events/test.txt', 'FS module of Node.js', () => log( 'File has written at the moment!' ) )
-Promise.resolve().then( () => log( 'Promise 1' ) )
+//! Close events
+fs.writeFile( './events/test.txt', 'FS module of Node.js', () => log( 'File has written at the moment!', timestamp() ) )
+//! nextTick - microtasks
+process.nextTick( () => log( 'NEXT TICK 1', timestamp() ) )
+//! microtasks - Promise
+Promise.resolve().then( () => log( 'Promise 1', timestamp() ) )
+//! nextTick - microtasks
+process.nextTick( () => log( 'NEXT TICK 2', timestamp() ) )
+//! Check phase - setImmediate
+setImmediate( () => log( 'SET IMMEDIATE', timestamp() ) )
 
-log('END')
+//! Poll phase - I/O
+dns.lookup( 'google.com', ( err, address, family ) =>
+{
+    if ( !err )
+    {
+        log( 'DNS' )
+        log( address )
+        log( family )
+    } else
+    {
+        log( err )
+    }
+} )
+//! __________end of ASYNC
+//! sync
+log( 'END', timestamp () )
